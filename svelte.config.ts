c31642561ad7a9f4de57898e60f3c673424ae6f1
Superchupu <53496941+SuperchupuDev@users.noreply.github.com>
2025-10-24 20:46:17 +0200

    add markdown library

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import type { Config } from '@sveltejs/kit';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { mdsvex } from 'mdsvex';
+import type { PreprocessorGroup } from 'svelte/compiler';
 
 export default {
+	extensions: ['.svelte', '.svx'],
+
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [vitePreprocess(), mdsvex() as PreprocessorGroup],
 
 	kit: {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
